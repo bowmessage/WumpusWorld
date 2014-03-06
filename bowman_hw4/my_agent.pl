33961e9fa54ca3b442d137e60d4e99d7920ceca7
James Bowman <bowmessage@gmail.com>
2014-03-06 02:29:38 -0600

    Done, using randomness however.

--- bowman_hw4/my_agent.pl
+++ bowman_hw4/my_agent.pl
@@ -11,16 +11,27 @@
 %
 % This is what should be fleshed out
 
-:- dynamic agent_x/1, agent_y/1, agent_ang/1, agent_gold/1, breeze/3, stench/3, glitter/3, bump/3, scream/3.
+:- dynamic agent_x/1, agent_y/1, agent_ang/1, agent_arrow/1, agent_gold/1, breeze/3, stench/3, glitter/3, bump/3, scream/3.
 
 init_agent:-
-	format('\n=====================================================\n'),
-	format('This is init_agent:\n\tIt gets called once, use it for your initialization\n\n'),
-	format('=====================================================\n\n'),
+	retractall(agent_x(_)),
+	retractall(agent_y(_)),
+	retractall(agent_gold(_)),
+	retractall(agent_ang(_)),
+	retractall(agent_arrow(_)),
+	retractall(breeze(_,_,_)),
+	retractall(stench(_,_,_)),
+	retractall(glitter(_,_,_)),
+	retractall(bump(_,_,_)),
+	retractall(scream(_,_,_)),
+
+
+
 	assert(agent_x(1)),
 	assert(agent_y(1)),
 	assert(agent_gold(0)),
-	assert(agent_ang(0)).
+	assert(agent_ang(0)),
+	assert(agent_arrow(1)).
 
 
 %run_agent(Percept,Action):-
@@ -37,7 +48,7 @@ run_agent(Percept, Action):-
 
 	update_percepts(Percept),
 
-	get_action(Action),
+	get_action(Action),!,
 
 	update_position(Action),
 
@@ -56,9 +67,13 @@ get_action(climb):-
 
 get_action(shoot):-
 	\+scream(yes,_,_), %no scream yet anywhere. Wumpus is still around!
-	agent_x(X), agent_y(Y).
+	agent_arrow(Ar),
+	Ar > 0,
+	format('maybeshoot?\n'),
+	agent_x(X), agent_y(Y), stench(yes,X,Y), format('asdf').
 
 get_action(goforward):-
+	random(10) > 4,
 	agent_x(X), agent_y(Y), agent_ang(A),
 
 	NewX is X + round(cos((A/360)*(pi*2))),
@@ -66,21 +81,45 @@ get_action(goforward):-
 
 	safe(NewX, NewY),  format('it is!\n'), valid(NewX,NewY), format('it is!\n').
 
-get_action(turnleft):- true.
+get_action(turnleft):- random(10) > 4. % random(2) returns 0 or 1.
+get_action(turnright):- true.
 
 safe(X,Y):-
 	format('Is ~d, ~d safe?\n', [X,Y]),
+	safe_breeze(X,Y),
+	safe_stench(X,Y).
+	
+
+safe_breeze(X,Y):-
+	format('Is ~d, ~d breeze-safe?\n', [X,Y]),
 	X1 is X + 1,
 	X0 is X - 1,
 	Y1 is Y + 1,
 	Y0 is Y - 1,
 	(
-		\+( breeze(yes,X1,Y) ;
-		breeze(yes,X0,Y) ;
-		breeze(yes,X,Y1) ;
-		breeze(yes,X,Y0) )
+		( breeze(no,X1,Y) ;
+		breeze(no,X0,Y) ;
+		breeze(no,X,Y1) ;
+		breeze(no,X,Y0) )
 		;
-		breeze(_,X,Y)
+		breeze(_,X,Y) %its safe if there's no breeze neighboring it or if we already know if there's a breeze there or not.
+	).
+
+safe_stench(X,Y):-
+	format('Is ~d, ~d stench-safe?\n', [X,Y]),
+	X1 is X + 1,
+	X0 is X - 1,
+	Y1 is Y + 1,
+	Y0 is Y - 1,
+	(
+		( stench(no,X1,Y) ;
+		stench(no,X0,Y) ;
+		stench(no,X,Y1) ;
+		stench(no,X,Y0) )
+		;
+		stench(_,X,Y) %its safe if there's no stench neighboring it or if we already know if there's a stench there or not.
+		;
+		scream(yes,_,_) % OR if the wumpus is dead :)
 	).
 
 /*
@@ -130,20 +169,27 @@ update_percepts([Stench,Breeze,Glitter,Bump,Scream]):-
 %   Percept = [Stench,Breeze,Glitter,Bump,Scream]
 %             The five parameters are either 'yes' or 'no'.
 
+update_position(climb):-
+	true.
+
+update_position(shoot):-
+	retract(agent_arrow(Ar)),
+	NewAr is Ar-1,
+	assert(agent_arrow(NewAr)).
+
 update_position(grab):-
-	agent_x(X), agent_y(Y), glitter(yes,X,Y),
 	retract(agent_gold(G)),
 	NewG is G + 1,
 	assert(agent_gold(NewG)).
 
 update_position(turnleft):-
 	retract(agent_ang(A)),
-	NewA is A + 90,
+	NewA is (A + 90) mod 360,
 	assert(agent_ang(NewA)).
 
 update_position(turnright):-
 	retract(agent_ang(A)),
-	NewA is A - 90,
+	NewA is (A - 90) mod 360,
 	assert(agent_ang(NewA)).
 
 update_position(goforward):-
